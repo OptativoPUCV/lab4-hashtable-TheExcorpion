1de756d3f11fff54b6f983844615823aef0c5b91
TheExcorpion <163429593+TheExcorpion@users.noreply.github.com>
2024-04-29 02:40:44 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -95,9 +95,23 @@ HashMap * createMap(long capacity)
 
 void eraseMap(HashMap * map,  char * key) 
 {
-  
-
-
+  if (map == NULL || key == NULL) 
+  {
+    return;
+  }
+  long posicion = hash(key, map->capacity);
+  while (map->buckets[posicion] != NULL &&
+         map->buckets[posicion]->key != NULL)
+    {
+    if (is_equal(map->buckets[posicion]->key, key)) 
+    {
+      map->buckets[posicion]->key = NULL;
+      map->buckets[posicion]->value = NULL;
+      map->size--;
+      return;
+    }
+    posicion = (posicion + 1) % map->capacity;
+  }
 }
 
 Pair * searchMap(HashMap * map,  char * key)
